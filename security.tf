a3307c7cce652eb99588d5a712dc13e08fba1ebb
Alfredo <mate.alfredo98@gmail.com>
2026-03-18 12:20:34 +0100

    fix: add vpc to security groups

--- security.tf
+++ security.tf
@@ -1,6 +1,10 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
 resource "aws_security_group" "ec2" {
   name = "app-ec2"
-
+  vpc_id = aws_vpc.main.id
   ingress {
     from_port   = 80
     to_port     = 80
@@ -19,7 +23,7 @@ resource "aws_security_group" "ec2" {
 
 resource "aws_security_group" "rds" {
   name = "app-rds"
-
+  vpc_id = aws_vpc.main.id
   ingress {
     from_port       = 3306
     to_port         = 3306
