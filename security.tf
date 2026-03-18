bb9d12a5094f43b9b37106fc1c7e1886ec428dc7
Alfredo <mate.alfredo98@gmail.com>
2026-03-18 13:35:21 +0100

    fix: egress add to ec2

--- security.tf
+++ security.tf
@@ -59,6 +59,13 @@ resource "aws_security_group" "ec2" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+}
 }
 
 resource "aws_security_group" "rds" {
