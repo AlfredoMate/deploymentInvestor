b63d047bf197e1c0ff5b12285680ac171ca69ff9
Alfredo <mate.alfredo98@gmail.com>
2026-03-18 12:53:53 +0100

    feat: add dns

--- security.tf
+++ security.tf
@@ -6,12 +6,14 @@ resource "aws_subnet" "subnet_a" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "eu-west-1a"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "subnet_b" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "eu-west-1b"
+  
 }
 
 resource "aws_db_subnet_group" "main" {
